59b6d73b8b79d83893a13e0c80bcff4219337512
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2015-03-01 23:22:05 +0100

    Directly load also translations

--- app/controllers/blogy/posts_controller.rb
+++ app/controllers/blogy/posts_controller.rb
@@ -6,7 +6,7 @@ module Blogy
 
     # GET /posts
     def index
-      @posts = Post.all
+      @posts = Post.joins(:translations).all
       respond_with @posts, root: :'blogy/posts'
     end
 
@@ -47,7 +47,7 @@ module Blogy
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_post
-        @post = Post.find(params[:id])
+        @post = Post.joins(:translations).find(params[:id])
       end
 
       # Only allow a trusted parameter "white list" through.
